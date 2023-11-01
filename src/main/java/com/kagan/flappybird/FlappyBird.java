f9f089fbc265e0a48f457c08acf91b369e7ac1ec
Kağan Yarımca <kyarimca48@gmail.com>
2023-11-01 15:44:59 +0300

    added graphics features

--- src/main/java/com/kagan/flappybird/FlappyBird.java
+++ src/main/java/com/kagan/flappybird/FlappyBird.java
@@ -1,17 +1,33 @@
 package com.kagan.flappybird;
 
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class FlappyBird {
+
+public class FlappyBird implements ActionListener {
     public static FlappyBird flappyBird;
     public final int WİDTH = 800, HEİGHT = 800;
+    public Renderer renderer;
 
     public FlappyBird(){
+
         JFrame jFrame = new JFrame();
+        renderer = new Renderer();
+        Timer timer = new Timer(20,this);
+
+        jFrame.add(renderer);
         jFrame.setSize(WİDTH, HEİGHT);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closing the window
+        jFrame.setResizable(false);
+        timer.start();
+
     }
-
-
+    public void repaint(Graphics g){
+        System.out.println("Hello");
+    }
 
     public static void main(String[] args) {
         flappyBird = new FlappyBird();
@@ -20,4 +36,8 @@ public class FlappyBird {
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        renderer.repaint();
+    }
 }
---
+++ src/main/java/com/kagan/flappybird/Renderer.java
@@ -0,0 +1,20 @@
+package com.kagan.flappybird;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Renderer  extends JPanel {
+
+    public static final long serialVersionUID  = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        FlappyBird.flappyBird.repaint(g);
+
+
+
+    }
+
+
+}
